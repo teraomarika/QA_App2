c070aa5d85e2de980c2a0b17452dce201c666471
hitorigoe <torigoe@seplus.jp>
2019-05-24 03:42:00 +0900

    9. 質問詳細画面前まで追加

--- app/src/main/java/com/example/qa_app/QuestionSendActivity.kt
+++ app/src/main/java/com/example/qa_app/QuestionSendActivity.kt
@@ -25,7 +25,6 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-
 import kotlinx.android.synthetic.main.activity_question_send.*
 
 import java.io.ByteArrayOutputStream
